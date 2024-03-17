2abee4938f1373a37c0916aa0404de3042831fed
Oliver Herklotz <oliver@hrkltz.io>
2024-03-17 11:18:45 +0100

    [*] Update.

--- src/component/app_component.ts
+++ src/component/app_component.ts
@@ -32,8 +32,9 @@ export class AppComponent extends LitElement {
 
 
     @state()
-    private _selectedElement: Element | null = null;
-    private _hoverElement: Element | null = null;
+    private _clickedElement: { element: Element | null, parentElement: Element | null } = { element: null, parentElement: null };
+    @state()
+    private _hoveredElement: { element: Element | null, parentElement: Element | null } = { element: null, parentElement: null };
     private _previousMouseEvent: MouseEvent | null = null;
     private _mousePositionX: number = 0;
     private _mousePositionY: number = 0;
@@ -44,7 +45,7 @@ export class AppComponent extends LitElement {
         return html`
             <h1>MeshUp</h1>
             <editor-component></editor-component>
-            <info-component selectedElementTagName=${this._hoverElement ? this._hoverElement.tagName : ''}></info-component>
+            <info-component selectedElementTagName=${this._hoveredElement.element ? this._hoveredElement.element.tagName : ''}></info-component>
         `;
     }
 
@@ -68,9 +69,7 @@ export class AppComponent extends LitElement {
 
 
     _onKeyDown(event: KeyboardEvent): void {
-        const targetElement = this._findTargetElement();
-
-        switch (targetElement.tagName) {
+        switch (this._hoveredElement.element!.tagName) {
             case 'EDITOR-COMPONENT':
                 switch (event.key) {
                     case 'ArrowUp':
@@ -90,22 +89,22 @@ export class AppComponent extends LitElement {
                         break;
                 };
                 break;
-            case 'NODE-COMPONENT':
+            case 'NODE-CORE-COMPONENT':
                 switch (event.key) {
                     case 'ArrowUp':
-                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, 0, -25);
+                        this._editorComponent!.moveNodeRelative(this._hoveredElement.parentElement as NodeComponent, 0, -25);
                         break;
                     case 'ArrowDown':
-                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, 0, 25);
+                        this._editorComponent!.moveNodeRelative(this._hoveredElement.parentElement as NodeComponent, 0, 25);
                         break;
                     case 'ArrowLeft':
-                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, -25, 0);
+                        this._editorComponent!.moveNodeRelative(this._hoveredElement.parentElement as NodeComponent, -25, 0);
                         break;
                     case 'ArrowRight':
-                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, 25, 0);
+                        this._editorComponent!.moveNodeRelative(this._hoveredElement.parentElement as NodeComponent, 25, 0);
                         break;
                     case 'd':
-                        this._editorComponent!.deleteNode(targetElement as NodeComponent);
+                        this._editorComponent!.deleteNode(this._hoveredElement.parentElement as NodeComponent);
                         break;
                 };
                 break;
@@ -114,50 +113,66 @@ export class AppComponent extends LitElement {
 
 
     _onMouseDown(event: MouseEvent): void {
-        this._selectedElement = this._findTargetElement();
+        this._clickedElement = this._findPointedElement();
         this._previousMouseEvent = event;
     }
 
 
     _onMouseMove(event: MouseEvent): void {
-        this._hoverElement = this._findTargetElement();
         this._mousePositionX = event.clientX;
         this._mousePositionY = event.clientY;
 
-        if (event.buttons !== 1) return;
-
-        switch (this._selectedElement!.tagName) {
-            case 'EDITOR-COMPONENT':
-                this._editorComponent!.transformRelative(this._mousePositionX  - this._previousMouseEvent!.clientX, this._mousePositionY - this._previousMouseEvent!.clientY);
+        switch (event.buttons) {
+            case 0:
+                this._hoveredElement = this._findPointedElement();
                 break;
-            case 'NODE-COMPONENT':
-                this._editorComponent!.moveNodeRelative(this._selectedElement! as NodeComponent, this._mousePositionX  - this._previousMouseEvent!.clientX, this._mousePositionY - this._previousMouseEvent!.clientY);
-                break;
-        };
+            case 1:
+                switch (this._clickedElement.element!.tagName) {
+                    case 'EDITOR-COMPONENT':
+                        this._editorComponent!.transformRelative(this._mousePositionX  - this._previousMouseEvent!.clientX, this._mousePositionY - this._previousMouseEvent!.clientY);
+                        break;
+                    case 'NODE-CORE-COMPONENT':
+                        this._editorComponent!.moveNodeRelative(this._clickedElement.parentElement! as NodeComponent, this._mousePositionX  - this._previousMouseEvent!.clientX, this._mousePositionY - this._previousMouseEvent!.clientY);
+                        break;
+                };
 
-        this._previousMouseEvent = event;
+                this._previousMouseEvent = event;
+                break;
+        }
     };
 
 
-    _onMouseUp(event: MouseEvent): void {
-        this._selectedElement = null;
+    _onMouseUp(_: MouseEvent): void {
+        this._clickedElement = { element: null, parentElement: null };
         this._previousMouseEvent = null;
     };
 
 
-    _findTargetElement(): Element {
-        this._selectedElement = this.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY);
+    // Note: parentElement crossing shadowRoot is not working.
+    _findPointedElement(): { element: Element | null, parentElement: Element | null } {
+        let pointedElement1 = this.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY)!;
 
-        if (this._selectedElement?.tagName === 'EDITOR-COMPONENT') {
-            let selectedSubComponent = this._selectedElement.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY);
+        switch (pointedElement1.tagName) {
+            case 'EDITOR-COMPONENT':
+                let pointedElement2 = pointedElement1.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY)!;
 
-            if (['NODE-COMPONENT'].includes(selectedSubComponent!.tagName)) {
-                let selectedSubSubComponent = selectedSubComponent!.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY);
-                this._selectedElement = selectedSubSubComponent;
-            };
+                switch (pointedElement2.tagName) {
+                    case 'NODE-COMPONENT':
+                        let pointedElement3 = pointedElement2.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY)!;
+                    
+                        switch (pointedElement3.tagName) {
+                            case 'NODE-INPUT-PORT-COMPONENT':
+                            case 'NODE-OUTPUT-PORT-COMPONENT':
+                            case 'NODE-CORE-COMPONENT':
+                                return { element: pointedElement3, parentElement: pointedElement2};
+                            default:
+                                return { element: pointedElement2, parentElement: pointedElement1};
+                        }
+                    default:
+                        return { element: pointedElement1, parentElement: null};
+                };
+            default:
+                return {element: null, parentElement: null}
         };
-
-        //console.log(this._mousePositionX, this._mousePositionY, this._selectedElement!.tagName);
-        return this._selectedElement!;
     }
 }
