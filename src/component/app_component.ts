2c39b80682ef099e7a6270270b45882d12fc42bd
Oliver Herklotz <oliver@hrkltz.io>
2024-03-17 01:21:25 +0100

    [*] Update.

---
+++ src/component/app_component.ts
@@ -0,0 +1,151 @@
+import { CSSResult, LitElement, css, html } from 'lit';
+import { customElement, query } from 'lit/decorators.js';
+import { EditorComponent } from './editor_component';
+import { NodeComponent } from './node_component';
+
+
+@customElement('app-component')
+export class AppComponent extends LitElement {
+    static override styles: CSSResult = css`
+        :host {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            height: 100%;
+        }
+
+        h1 {
+            flex: 0 0 auto;
+        }
+
+        editor-component {
+            flex: 1 1 auto;
+        }
+    `;
+
+
+    private _selectedElement: Element | null = null;
+    private _previousMouseEvent: MouseEvent | null = null;
+    private _mousePositionX: number = 0;
+    private _mousePositionY: number = 0;
+    @query('editor-component', true) private _editorComponent!: EditorComponent;
+    
+
+    override render() {
+        return html`
+            <h1>MeshUp</h1>
+            <editor-component></editor-component>
+        `;
+    }
+
+
+    connectedCallback(): void {
+        super.connectedCallback();
+        window.addEventListener('mousedown', this._onMouseDown.bind(this));
+        window.addEventListener('mousemove', this._onMouseMove.bind(this));
+        window.addEventListener('mouseup', this._onMouseUp.bind(this));
+        window.addEventListener('keydown', this._onKeyDown.bind(this));
+        window.addEventListener('contextmenu', (event) => { event.preventDefault(); });
+    }
+
+    // For the moment not needed as the AppComponent is the root one.
+    //disconnectedCallback(): void {
+    //    window.removeEventListener('mousedown', this._onMouseDown);
+    //    window.removeEventListener('mousemove', this._onMouseMove);
+    //    window.removeEventListener('keydown', this._onKeyDown);
+    //    super.disconnectedCallback();
+    //}
+
+
+    _onKeyDown(event: KeyboardEvent): void {
+        const targetElement = this._findTargetElement();
+
+        switch (targetElement.tagName) {
+            case 'EDITOR-COMPONENT':
+                switch (event.key) {
+                    case 'ArrowUp':
+                        this._editorComponent!.transformRelative(0, 25);
+                        break;
+                    case 'ArrowDown':
+                        this._editorComponent!.transformRelative(0, -25);
+                        break;
+                    case 'ArrowLeft':
+                        this._editorComponent!.transformRelative(25, 0);
+                        break;
+                    case 'ArrowRight':
+                        this._editorComponent!.transformRelative(-25, 0);
+                        break;
+                    case 'a':
+                        this._editorComponent!.addNode(this._mousePositionX, this._mousePositionY);
+                        break;
+                };
+                break;
+            case 'NODE-COMPONENT':
+                switch (event.key) {
+                    case 'ArrowUp':
+                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, 0, -25);
+                        break;
+                    case 'ArrowDown':
+                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, 0, 25);
+                        break;
+                    case 'ArrowLeft':
+                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, -25, 0);
+                        break;
+                    case 'ArrowRight':
+                        this._editorComponent!.moveNodeRelative(targetElement as NodeComponent, 25, 0);
+                        break;
+                    case 'd':
+                        this._editorComponent!.deleteNode(targetElement as NodeComponent);
+                        break;
+                };
+                break;
+        };
+    }
+
+
+    _onMouseDown(event: MouseEvent): void {
+        this._selectedElement = this._findTargetElement();
+        this._previousMouseEvent = event;
+    }
+
+
+    _onMouseMove(event: MouseEvent): void {
+        this._mousePositionX = event.clientX;
+        this._mousePositionY = event.clientY;
+
+        if (event.buttons !== 1) return;
+
+        switch (this._selectedElement!.tagName) {
+            case 'EDITOR-COMPONENT':
+                this._editorComponent!.transformRelative(this._mousePositionX  - this._previousMouseEvent!.clientX, this._mousePositionY - this._previousMouseEvent!.clientY);
+                break;
+            case 'NODE-COMPONENT':
+                this._editorComponent!.moveNodeRelative(this._selectedElement! as NodeComponent, this._mousePositionX  - this._previousMouseEvent!.clientX, this._mousePositionY - this._previousMouseEvent!.clientY);
+                break;
+        };
+
+        this._previousMouseEvent = event;
+    };
+
+
+    _onMouseUp(event: MouseEvent): void {
+        this._selectedElement = null;
+        this._previousMouseEvent = null;
+    };
+
+
+    _findTargetElement(): Element {
+        this._selectedElement = this.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY);
+
+        if (this._selectedElement?.tagName === 'EDITOR-COMPONENT') {
+            let selectedSubComponent = this._selectedElement.shadowRoot!.elementFromPoint(this._mousePositionX, this._mousePositionY);
+
+            if (['NODE-COMPONENT'].includes(selectedSubComponent!.tagName)) {
+                this._selectedElement = selectedSubComponent;
+            };
+        };
+
+        //console.log(this._mousePositionX, this._mousePositionY, this._selectedElement!.tagName);
+        return this._selectedElement!;
+    }
+}
--- src/component/editor_component.ts
+++ src/component/editor_component.ts
@@ -1,51 +1,26 @@
 import { CSSResult, LitElement, TemplateResult, css, html } from 'lit';
-import { customElement, property, query } from 'lit/decorators.js';
+import { customElement, query } from 'lit/decorators.js';
 import { NodeComponent } from './node_component';
 
 
 @customElement('editor-component')
 export class EditorComponent extends LitElement {
     static override styles: CSSResult = css`
-    :host,
-    svg {
-        width: 100%;
-        height: 100%;
-        box-sizing: border-box;
-    }
-
-    svg {
-        border: 1px dashed black;
-    }
-
-
-    :host:focus {
-        outline: none;
-        background-color: red;
-        border-color: red;
-        border-style: solid;
-    }
-
-    svg:focus {
-        background-color: transparent;
-        outline: none;
-        border-color: blue;
-        border-style: solid;
-    }
+        :host,
+        svg {
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+        }
     `;
 
-    _isDragging: boolean = false;
-    _mousePositionX: number = 0;
-    _mousePositionY: number = 0;
     _offsetX: number = 0;
     _offsetY: number = 0;
-    _previousEvent: MouseEvent | null = null;
     _zoom: number = 1.0;
-    _focus: Element | null = null;
-    @query('g#transformer') _transformer!: SVGElement;
-    @query('svg') _svg!: SVGElement;
+    @query('g#transformer', true) _transformer!: SVGElement;
+    @query('svg', true) _svg!: SVGElement;
 
 
-    // Note: tabindex is required to make the svg element focusable which allows it to react to key events. Somehow it can't be set in the property decorator but it works if the first child has it.
     override render(): TemplateResult {
         return html`
         <svg
@@ -58,64 +33,8 @@ export class EditorComponent extends LitElement {
         `;
     }
 
-    oncontextmenu: ((this: GlobalEventHandlers, ev: MouseEvent) => any) = (event: MouseEvent) => {
-        event.preventDefault();
-    };
-    onfocus: ((this: GlobalEventHandlers, ev: FocusEvent) => any) = (event: FocusEvent) => {
-        console.log('EditorComponent.onfocus');
-        this._focus = null;
-        window.addEventListener('meshup-focus', this._windowOnmeshupfocus);
-    };
-    onblur: ((this: GlobalEventHandlers, ev: FocusEvent) => any) = (event: FocusEvent) => {
-        console.log('EditorComponent.onblur');
-        window.removeEventListener('meshup-focus', this._windowOnmeshupfocus);
-    };
-    onmousedown: ((this: GlobalEventHandlers, ev: MouseEvent) => any) = (event: MouseEvent) => {
-        console.log('EditorComponent.onmousedown');
-        switch (event.button) {
-            // Left mouse button.
-            case 0:
-                this._previousEvent = event;
-                this._isDragging = true;
-                this.requestUpdate();
-                break;
-            // Middle mouse button.
-            case 1:
-                this._offsetX = 0;
-                this._offsetY = 0;
-                this._zoom = 1.0;
-                this.requestUpdate();
-                break;
-        }
-    }
-    onmousemove: ((this: GlobalEventHandlers, ev: MouseEvent) => any) = (event: MouseEvent) => {
-        console.log('EditorComponent.onmousemove');
-        this._mousePositionX = event.clientX;
-        this._mousePositionY = event.clientY;
 
-        if (!this._isDragging) return;
-
-        if (this._focus === null) {
-            this._offsetX += event.clientX - this._previousEvent!.clientX;
-            this._offsetY += event.clientY - this._previousEvent!.clientY;
-            this.requestUpdate();
-        } else {
-            const foreignObject = this._focus.parentElement;
-            const x = Number(foreignObject!.getAttribute('x'));
-            const y = Number(foreignObject!.getAttribute('y'));
-            foreignObject!.setAttribute('x', `${x + (event.clientX - this._previousEvent!.clientX)/this._zoom}`);
-            foreignObject!.setAttribute('y', `${y + (event.clientY - this._previousEvent!.clientY)/this._zoom}`);
-        }
-
-        this._previousEvent = event;
-    }
-    onmouseup: ((this: GlobalEventHandlers, ev: MouseEvent) => any) = (event: MouseEvent) => {
-        console.log('EditorComponent.onmouseup');
-        this._isDragging = false;
-        this._previousEvent = null;
-        this.requestUpdate();
-    }
-    onwheel: ((this: GlobalEventHandlers, ev: WheelEvent) => any) = (event: WheelEvent) => {
+    public onwheel: ((this: GlobalEventHandlers, ev: WheelEvent) => any) = (event: WheelEvent) => {
         // Prevent scrolling
         event.preventDefault();
         let tmpZoom = this._zoom + event.deltaY*0.001;
@@ -124,94 +43,51 @@ export class EditorComponent extends LitElement {
         else if (tmpZoom >= 1.5) this._zoom = 1.5;
         else this._zoom = tmpZoom;
         this.requestUpdate();
+    };
+
+
+    public onkeydown: ((this: GlobalEventHandlers, ev: KeyboardEvent) => any) = (event: KeyboardEvent) => {
+        console.log('EditorComponent.onkeydown', event.key);
+        event.stopPropagation();
+    };
+
+
+    public transformRelative(x: number, y: number) {
+        this._offsetX += x/this._zoom;
+        this._offsetY += y/this._zoom;
+        this.requestUpdate();
     }
-    onkeydown: ((this: GlobalEventHandlers, ev: KeyboardEvent) => any) = (event: KeyboardEvent) => {
-        if (this._focus === null) {
-            switch (event.key) {
-                case 'ArrowUp':
-                    this._offsetY += 25;
-                    this.requestUpdate();
-                break;
-                case 'ArrowDown':
-                    this._offsetY -= 25;
-                    this.requestUpdate();
-                break;
-                case 'ArrowLeft':
-                    this._offsetX += 25;
-                    this.requestUpdate();
-                break;
-                case 'ArrowRight':
-                    this._offsetX -= 25;
-                    this.requestUpdate();
-                break;
-                case 'Home':
-                    this._offsetX = 0;
-                    this._offsetY = 0;
-                    this._zoom = 1.0;
-                    this.requestUpdate();
-                    break;
-                case 'a':
-                    if (!event.ctrlKey) return; 
+
+
+    public addNode(x: number, y: number) {
+        // Get the cursor position relative to the start point of the svg element.
+        let svgRect = this._svg.getBoundingClientRect()!;
+        let cX: number = ((x - this._offsetX) - svgRect.left)/this._zoom;
+        let cY: number = ((y - this._offsetY) - svgRect.top)/this._zoom;
+        // Create a new node component and append it to the transformer.
+        const nodeComponent = new NodeComponent();
+        let foreignObject = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
+        foreignObject.style.boxSizing = 'border-box';
+        foreignObject.setAttribute('x', String(cX-55));
+        foreignObject.setAttribute('y', String(cY-55));
+        foreignObject.setAttribute('width', nodeComponent.width);
+        foreignObject.setAttribute('height', nodeComponent.height);
+        foreignObject.appendChild(nodeComponent);
+        this._transformer.appendChild(foreignObject);
+    }
+
     
-                    // Get the cursor position relative to the start point of the svg element.
-                    let svgRect = this._svg.getBoundingClientRect()!;
-                    let cX: number = ((this._mousePositionX - this._offsetX) - svgRect.left)/this._zoom;
-                    let cY: number = ((this._mousePositionY - this._offsetY) - svgRect.top)/this._zoom;
-                    // Create a new node component and append it to the transformer.
-                    const nodeComponent = new NodeComponent();
-                    let foreignObject = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
-                    foreignObject.style.boxSizing = 'border-box';
-                    foreignObject.setAttribute('x', String(cX-55));
-                    foreignObject.setAttribute('y', String(cY-55));
-                    foreignObject.setAttribute('width', nodeComponent.width);
-                    foreignObject.setAttribute('height', nodeComponent.height);
-                    foreignObject.appendChild(nodeComponent);
-                    this._transformer.appendChild(foreignObject);
-                break;
-            };
-
-            return;
-        }
-
-        switch (this._focus.tagName.toLowerCase()) {
-            case 'node-component':
-                return this._onkeydownNodeComponentHandler(event);
-        };
+    public moveNodeRelative(nodeComponent: NodeComponent, dX: number, dY: number) {
+        const foreignObject = nodeComponent.parentElement;
+        const x = Number(foreignObject!.getAttribute('x'));
+        const y = Number(foreignObject!.getAttribute('y'));
+        foreignObject!.setAttribute('x', `${x + dX/this._zoom}`);
+        foreignObject!.setAttribute('y', `${y + dY/this._zoom}`);
     }
-    
-    _windowOnmeshupfocus: ((this: Window, ev: any) => any) = (event: CustomEvent) => {
-        console.log('EditorComponent._windowOnmeshupfocus');
-        this._focus = event.detail;
-    }
-    _onkeydownNodeComponentHandler: ((this: GlobalEventHandlers, ev: KeyboardEvent) => any) = (event: KeyboardEvent) => {
-        switch (event.key) {
-            case 'ArrowUp': {
-                const foreignObject = this._focus!.parentElement;
-                const y = Number(foreignObject!.getAttribute('y'));
-                foreignObject!.setAttribute('y', `${y - 25}`);
-            } break;
-            case 'ArrowDown': {
-                const foreignObject = this._focus!.parentElement;
-                const y = Number(foreignObject!.getAttribute('y'));
-                foreignObject!.setAttribute('y', `${y + 25}`);
-            } break;
-            case 'ArrowLeft': {
-                const foreignObject = this._focus!.parentElement;
-                const x = Number(foreignObject!.getAttribute('x'));
-                foreignObject!.setAttribute('x', `${x - 25}`);}
-            break;
-            case 'ArrowRight': {
-                const foreignObject = this._focus!.parentElement;
-                const x = Number(foreignObject!.getAttribute('x'));
-                foreignObject!.setAttribute('x', `${x + 25}`);
-            } break;
-            case 'd':
-                if (!event.ctrlKey) return;
 
-                const foreignObject = this._focus!.parentElement;
-                this._transformer.removeChild(foreignObject!);
-                this.focus();
-            break;
-        };
+
+    public deleteNode(nodeComponent: NodeComponent) {
+        const foreignObject = nodeComponent.parentElement;
+        this._transformer.removeChild(foreignObject!);
     }
-}
+};
